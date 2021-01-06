d1111f04b8e18197a2f541fcdb6efb37500b7a94
Sociosarbis <136657577@qq.com>
2021-01-07 01:55:20 +0800

    feat: :sparkles: calc_equations

--- src/calc_equation.rs
+++ src/calc_equation.rs
@@ -1,69 +1,82 @@
 use super::Solution;
 use std::collections::hash_map::HashMap;
+use std::cell::RefCell;
+
 
 impl Solution {
-  /*pub fn calc_equation(equations: Vec<Vec<String>>, values: Vec<f64>, queries: Vec<Vec<String>>) -> Vec<f64> {
-    let mut graph: HashMap<String, HashMap<String, f64>> = HashMap::new();
+
+  pub fn calc_equation(equations: Vec<Vec<String>>, values: Vec<f64>, queries: Vec<Vec<String>>) -> Vec<f64> {
+    let mut graph: HashMap<&String, RefCell<HashMap<String, f64>>> = HashMap::new();
+    let mut rel: HashMap<&String, RefCell<String>> = HashMap::new();
     let mut ret: Vec<f64> = vec![];
     for i in 0..equations.len() {
-      if !graph.contains_key(&equations[i][0]) {
-        graph.insert(equations[i][0].to_owned(), HashMap::new());
-      }
-      if !graph.contains_key(&equations[i][1]) {
-        graph.insert(equations[i][1].to_owned(), HashMap::new());
-      }
-      let r1 = graph.get_mut(&equations[i][0]).unwrap();
-      r1.insert(equations[i][1].to_owned(), 1.0 / values[i]);
-      let r2 = graph.get_mut(&equations[i][1]).unwrap();
-      r2.insert(equations[i][0].to_owned(), values[i]);
-    }
-    let mut visited: HashMap<String, bool> = HashMap::new();
-    for pair in &graph {
-      visited.insert(pair.0.to_owned(), false);
-    }
-    let mut bfs: Vec<(&String, f64)> = vec![];
-    let mut new_visited: HashMap<String, bool>; 
-    for q in &queries {
-      let s = &q[1];
-      let t = &q[0];
-      let n = ret.len();
-      new_visited = visited.clone();
-      if let Some(is_visited) = new_visited.get_mut(s) {
-        *is_visited = true;
-        bfs.push((s, 1.0));
-        while !bfs.is_empty() {
-          if let Some((cur, base)) = bfs.pop() {
-            if let Some(res) = graph.get(cur) {
-              if res.contains_key(t) {
-                bfs.clear();
-                ret.push(*(res.get(t).unwrap()) * base);
-              } else {
-                for it in res {
-                  if let Some(v) = new_visited.get_mut(it.0) {
-                    if *v == true {
-                      continue
-                    } else {
-                      *v = true;
-                    }
-                  }
-                  bfs.push((it.0, base * *it.1));
-                }
+      let s = &equations[i][0];
+      let t = &equations[i][1];
+      if let Some(g1) = rel.get(s) {
+        if let Some(g2) = rel.get(t) {
+          if g2.borrow().to_owned() != g1.borrow().to_owned() {
+            {
+              let mut m1 = graph.get(&g1.borrow().to_owned()).unwrap().borrow_mut();
+              let m2 = graph.get(&g2.borrow().to_owned()).unwrap().borrow().to_owned();
+              let base = m1.get(s).unwrap() * values[i] / m2.get(t).unwrap();
+              for p in m2 {
+                *rel.get(&p.0).unwrap().borrow_mut() = g1.borrow().to_owned();
+                m1.insert(p.0, p.1 * base);
               }
+            }
+          }
+          graph.remove(t);
+          continue;
+        }
+      }
+      let g2 = {
+        if rel.contains_key(t) {
+          rel.get(t).unwrap().borrow().to_owned()
+        } else {
+          String::new()
+        }
+      };
+      if g2.len() != 0 {
+        rel.insert(s, RefCell::new(g2.to_owned()));
+        let val = {
+          *graph.get(&g2).unwrap().borrow().get(t).unwrap() / values[i]
+        };
+        graph.get(&g2).unwrap().borrow_mut().insert(s.to_owned(), val);
+      } else {
+        let g1 = {
+          if !rel.contains_key(s) {
+            rel.insert(s, RefCell::new(s.to_owned()));
+            let mut new_map: HashMap<String, f64> = HashMap::new();
+            new_map.insert(s.to_owned(), 1.0);
+            graph.insert(s, RefCell::new(new_map));
+            s.to_owned()
+          } else {
+            rel.get(s).unwrap().borrow().to_owned()
+          }
+        };
+        rel.insert(t, RefCell::new(g1.to_owned()));
+        let val = {
+          *graph.get(&g1).unwrap().borrow().get(s).unwrap() * values[i]
+        };
+        graph.get(&g1).unwrap().borrow_mut().insert(t.to_owned(), val);
+      }
+    }
+    for q in queries {
+      let s = &q[0];
+      let t = &q[1];
+      if rel.contains_key(s) && rel.contains_key(t) {
+        if let Some(g1) = rel.get(s) {
+          if let Some(g2) = rel.get(t) {
+            if g1.borrow().to_owned() == g2.borrow().to_owned() {
+              let g = g1.borrow().to_owned();
+              ret.push(graph.get(&g).unwrap().borrow().get(t).unwrap() / graph.get(&g).unwrap().borrow().get(s).unwrap());
+              continue;
             }
           }
         }
       }
-      if ret.len() == n {
-        ret.push(-1.0);
-      }
+      ret.push(-1.0);
     }
-    ret
-  }*/
-
-  pub fn calc_equation(equations: Vec<Vec<String>>, values: Vec<f64>, queries: Vec<Vec<String>>) -> Vec<f64> {
-    let mut graph: HashMap<String, HashMap<String, f64>> = HashMap::new();
-    let mut rel: HashMap<String, String> = HashMap::new();
-    let mut ret: Vec<f64> = vec![];
     ret
   }
 }
@@ -110,6 +123,24 @@ mod tests {
         queries: t(&[&["a","b"],&["b","a"],&["a","c"],&["x","y"]]),
         ret: vec![0.50000,2.00000,-1.00000,-1.00000]
       },
+      Suite {
+        equations: t(&[&["a","e"],&["b","e"]]),
+        values: vec![4.0,3.0],
+        queries: t(&[&["a","b"],&["e","e"],&["x","x"]]),
+        ret: vec![1.3333333333333333,1.00000,-1.00000]
+      },
+      Suite {
+        equations: t(&[&["a","b"],&["e","f"],&["b","e"]]),
+        values: vec![3.4,1.4,2.3],
+        queries: t(&[&["b","a"],&["a","f"],&["f","f"],&["e","e"],&["c","c"],&["a","c"],&["f","e"]]),
+        ret: vec![0.29411764705882354, 10.947999999999999, 1.0, 1.0, -1.0, -1.0, 0.7142857142857143]
+      },
+      Suite {
+        equations: t(&[&["a","b"],&["b","c"],&["a","c"]]),
+        values: vec![2.0,3.0,6.0],
+        queries: t(&[&["a","c"],&["b","a"],&["a","e"],&["a","a"],&["x","x"]]),
+        ret: vec![6.00000,0.50000,-1.00000,1.00000,-1.00000]
+      }
     ];
 
     for s in suites {
