f9722853e1673797fc5ee24be5f9d6333052f192
sociosarbis <136657577@qq.com>
2021-08-14 01:59:48 +0800

    feat: :sparkles: (Recursion Math Dynamic Programming) 233. Number of Digit One

--- src/count_digit_one.rs
+++ src/count_digit_one.rs
@@ -1,21 +1,37 @@
 use super::Solution;
 
+use std::collections::HashMap;
 
 impl Solution {
   pub fn count_digit_one(n: i32) -> i32 {
     if n == 0 {
       return 0;
     }
-    let mut dp: Vec<i32> = vec![0;n as usize + 1];
-    let mut base: i32 = 1;
-    dp[1] = 1;
-    for i in 2..n + 1 {
-      if i == base * 10 {
-        base *= 10;
-      }
-      dp[i as usize] = dp[i as usize % base as usize] + if i / base == 1 { 1 } else { 0 };
+    if n < 10 {
+      return 1;
     }
-    dp.iter().fold(0, |acc, item| { acc + item })
+    let mut base = 1;
+    let mut num = n / 10;
+    let mut dp: HashMap<i32, i32> = HashMap::new();
+    dp.insert(0, 0);
+    for i in 1..10 {
+      dp.insert(i, 1);
+    }
+    let mut count = n % 10;
+    while num > 0 {
+      let c = num % 10;
+      num /= 10;
+      base *= 10;
+      let rest = count;
+      count += c * base;
+      if !dp.contains_key(&count) {
+        dp.insert(count, c * dp.get(&(base - 1)).unwrap() + dp.get(&rest).unwrap() + if c > 1 { base } else { rest + 1 });
+      }
+      if num > 0 {
+        dp.insert(base * 10 - 1, 10 * dp.get(&(base - 1)).unwrap() + base);
+      }
+    }
+    *dp.get(&n).unwrap()
   }
 }
 
@@ -38,6 +54,10 @@ mod tests {
       Suite {
         n: 0,
         ret: 0
+      },
+      Suite {
+        n: 1000000000,
+        ret: 900000001
       }
     ];
 
