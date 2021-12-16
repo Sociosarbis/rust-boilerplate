4df7fa9d087d4c97219afa5965f90b4255477a9f
sociosarbis <136657577@qq.com>
2021-12-16 23:36:23 +0800

    feat: :sparkles: (Geometry Array Math Sorting Sliding Window) 1610. Maximum Number of Visible Points

--- part_2/src/visible_points.rs
+++ part_2/src/visible_points.rs
@@ -1,8 +1,39 @@
+use std::f64::consts::PI;
+
 use super::*;
 
 impl Solution {
   pub fn visible_points(points: Vec<Vec<i32>>, angle: i32, location: Vec<i32>) -> i32 {
-
+    let mut angles: Vec<f64> = points.iter().filter_map(|p| {
+      if p[1] == location[1] && p[0] == location[0] {
+        None
+      } else {
+        let res = ((p[1] - location[1]) as f64).atan2((p[0] - location[0]) as f64);
+        Some(if res < 0.0 { res + PI * 2.0 } else { res })
+      }
+    }).collect();
+    angles.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+    let angle_d = (angle as f64) / 180.0 * PI;
+    let mut j = 0;
+    let mut ret = 1;
+    for i in 0..angles.len() {
+      let start = angles[i];
+      while j + 2 < angles.len() * 2 {
+        let end = if j + 1 >= angles.len() { angles[j + 1 - angles.len()] + PI * 2.0 } else { angles[j + 1] }; 
+        if end - start <= angle_d {
+          j += 1;
+        } else {
+          break;
+        }
+      }
+      if j - i + 1 > ret {
+        ret = j - i + 1;
+      }
+      if j + 2 >= angles.len() * 2 {
+        break;
+      }
+    }
+    (ret + (points.len() -  angles.len())) as i32
   }
 }
 
