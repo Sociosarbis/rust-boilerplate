2bae72240683c59a61713a5fe496c9b62da3f65c
sociosarbis <136657577@qq.com>
2021-12-20 22:23:59 +0800

    feat: :sparkles: (Array Two Pointers Binary Search Sorting) 475. Heaters

--- part_2/src/find_radius.rs
+++ part_2/src/find_radius.rs
@@ -1,9 +1,24 @@
 use super::*;
 
+use std::cmp::min;
+
 impl Solution {
   pub fn find_radius(mut houses: Vec<i32>, mut heaters: Vec<i32>) -> i32 {
     houses.sort_unstable();
     heaters.sort_unstable();
+    let mut ret = 0;
+    let mut j = 0;
+    for house in houses {
+      while j + 1 < heaters.len() && heaters[j + 1] <= house {
+        j += 1;
+      }
+      let right = if heaters[j] < house && j + 1 < heaters.len() { heaters[j + 1] } else { heaters[j] };
+      let dist = min((heaters[j] - house).abs(), (right - house).abs());
+      if dist > ret {
+        ret = dist;
+      }
+    }
+    ret
   }
 }
 
@@ -21,7 +36,7 @@ mod tests {
   #[test]
   fn test_find_radius_simple() {
     let suites = vec![
-      Suite {
+      /*Suite {
         houses: vec![1,2,3],
         heaters: vec![2],
         ret: 1
@@ -35,6 +50,11 @@ mod tests {
         houses: vec![1,5],
         heaters: vec![2],
         ret: 3
+      },*/
+      Suite {
+        houses: vec![282475249,622650073,984943658,144108930,470211272,101027544,457850878,458777923],
+        heaters: vec![823564440,115438165,784484492,74243042,114807987,137522503,441282327,16531729,823378840,143542612],
+        ret: 161834419
       }
     ];
 
