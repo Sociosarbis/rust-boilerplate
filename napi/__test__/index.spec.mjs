41e34687fe465709b52ee3c503e908b2bbe11c64
Sociosarbis <136657577@qq.com>
2023-05-18 01:48:38 +0800

    feat: :sparkles: distance

--- napi/__test__/index.spec.mjs
+++ napi/__test__/index.spec.mjs
@@ -1,8 +1,12 @@
 import test from 'ava'
 
-import { sum } from '../index.js'
+import { sum, distance } from '../index.js'
 
 test('sum from native', (t) => {
   const obj = { value: 1 }
   t.is(sum(obj, 2), 5)
+})
+
+test('distance from native', (t) => {
+  t.is(distance([3, 4], [0, 0]), 5)
 })
--- napi/src/lib.rs
+++ napi/src/lib.rs
@@ -5,7 +5,15 @@ use napi::{Env, JsNumber, JsObject, JsString, NapiValue};
 #[macro_use]
 extern crate napi_derive;
 
-fn get<T>(env: Env, mut obj: JsObject, paths: &Vec<&str>) -> Option<T>
+fn result_to_option<T, E>(result: Result<T, E>) -> Option<T> {
+  if let Ok(v) = result {
+    Some(v)
+  } else {
+    None
+  }
+}
+
+fn get<T>(env: Env, obj: &JsObject, paths: &Vec<&str>) -> Option<T>
 where
   T: NapiValue,
 {
@@ -13,37 +21,68 @@ where
     return None;
   }
   let n = paths.len();
-  for &p in paths.iter().take(n - 1) {
-    obj = obj
-      .get_property::<JsString, JsObject>(env.create_string(p).unwrap())
-      .unwrap();
+  if n == 1 {
+    if let Ok(cur) = obj.get_property::<JsString, T>(env.create_string(paths[0]).unwrap()) {
+      return Some(cur);
+    }
+  } else if let Ok(mut cur) =
+    obj.get_property::<JsString, JsObject>(env.create_string(paths[0]).unwrap())
+  {
+    for &p in paths.iter().skip(1).take(n - 2) {
+      if let Ok(next) = cur.get_property::<JsString, JsObject>(env.create_string(p).unwrap()) {
+        cur = next;
+      } else {
+        break;
+      }
+    }
+    return result_to_option(
+      cur.get_property::<JsString, T>(env.create_string(paths[n - 1]).unwrap()),
+    );
   }
-  Some(
-    obj
-      .get_property::<JsString, T>(env.create_string(paths[n - 1]).unwrap())
-      .unwrap(),
-  )
+  None
 }
 
-fn set<T>(env: Env, mut obj: JsObject, paths: &Vec<&str>, value: T) -> JsObject
+fn set<T>(env: Env, obj: &mut JsObject, paths: &Vec<&str>, value: T) -> napi::Result<()>
 where
   T: NapiValue,
 {
   if paths.is_empty() {
-    return obj;
+    return Ok(());
   }
   let n = paths.len();
-  for &p in paths.iter().take(n - 1) {
-    obj = obj
-      .get_property::<JsString, JsObject>(env.create_string(p).unwrap())
-      .unwrap();
+  if n == 1 {
+    obj.set_property(env.create_string(paths[0]).unwrap(), value)?;
+  } else if let Ok(mut cur) =
+    obj.get_property::<JsString, JsObject>(env.create_string(paths[0]).unwrap())
+  {
+    for &p in paths.iter().skip(1).take(n - 2) {
+      cur = cur
+        .get_property::<JsString, JsObject>(env.create_string(p).unwrap())
+        .unwrap();
+    }
+    cur.set_property(env.create_string(paths[n - 1]).unwrap(), value)?;
   }
-  let _ = obj.set_property(env.create_string(paths[n - 1]).unwrap(), value);
-  obj
+  Ok(())
 }
 
 #[napi]
-pub fn sum(env: Env, mut a: JsObject, b: i32) -> i32 {
-  a = set(env, a, &vec!["value"], env.create_int32(3).unwrap());
-  i32::try_from(get::<JsNumber>(env, a, &vec!["value"]).unwrap()).unwrap() + b
+pub fn sum(env: Env, mut a: JsObject, b: i32) -> napi::Result<i32> {
+  set(env, &mut a, &vec!["value"], env.create_int32(3).unwrap())?;
+  Ok(i32::try_from(get::<JsNumber>(env, &a, &vec!["value"]).unwrap())? + b)
+}
+
+#[napi(ts_args_type = "a: number[], b: number[]")]
+pub fn distance(env: Env, a: JsObject, b: JsObject) -> napi::Result<f64> {
+  if let Ok(n1) = a.get_array_length() {
+    if let Ok(n2) = b.get_array_length() {
+      if n1 >= 2 && n2 >= 2 {
+        let x1 = f64::try_from(get::<JsNumber>(env, &a, &vec!["0"]).unwrap())?;
+        let y1 = f64::try_from(get::<JsNumber>(env, &a, &vec!["1"]).unwrap())?;
+        let x2 = f64::try_from(b.get_element::<JsNumber>(0)?)?;
+        let y2 = f64::try_from(b.get_element::<JsNumber>(1)?)?;
+        return Ok(((x1 - x2).powf(2.0) + (y1 - y2).powf(2.0)).sqrt());
+      }
+    }
+  }
+  Err(napi::Error::from_status(napi::Status::InvalidArg))
 }
